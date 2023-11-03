d795997f0273d47230371aa94070fa64395c323c
vrindatyagi <vrindatyagi09@gmail.com>
2023-11-03 12:57:03 +0530

    some fixes

--- src/components/loadingScreenDown.js
+++ src/components/loadingScreenDown.js
@@ -4,13 +4,13 @@ const LoadingScreenDown = () => {
   return (
     <div className="h-screen w-screen rounded-t-3xl">
       <div className="absolute left-36 top-36 h-[23.65px] w-[108px]">
-        <img src={'/assets/green-logo.svg'} alt='green-logo'></img>
+        <img src={'/assets/green-logo.svg'} alt="green-logo"></img>
       </div>
-      <div className="absolute bottom-0 h-[436px] w-[390px] rotate-180 rounded-bl-[30px] rounded-br-[170px] rounded-tl-3xl rounded-tr-3xl bg-[#5ECC7B]">
+      <div className="absolute bottom-0 h-[436px] w-[390px] rotate-180 rounded-bl-[30px] rounded-br-[170px] bg-[#5ECC7B]">
         <img
           className="absolute left-32 top-52 h-56 rotate-180 opacity-20"
           src={'/assets/tlogo.svg'}
-          alt='tlogo'
+          alt="tlogo"
         ></img>
         <div className="absolute left-8 top-[189px] h-[120px] w-[258px] rotate-180 font-['SF_Pro_Text'] text-[50.67px] font-bold leading-[60.46px] tracking-[-0.54px] text-[#0A3A17]">
           Fuelling your journey
--- src/components/loadingScreenUp.js
+++ src/components/loadingScreenUp.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 const LoadingScreenUp = () => {
   return (
-    <div className="w-[390px] h-[844px] relative bg-black rounded-[33px]">
-      <div className="h-1/2 w-96 rounded-bl-[30px] rounded-br-[170px] rounded-tl-[33px] rounded-tr-[33px] bg-[#FA5757]">
+    <div className="relative h-screen w-screen overflow-hidden bg-black">
+      <div className="h-1/2 w-96 rounded-bl-[30px] rounded-br-[170px] bg-[#FA5757]">
         <img
           className="absolute left-40 top-56 h-56 opacity-40"
           src={'/assets/tlogo.svg'}
-          alt='tlogo'
+          alt="tlogo"
         ></img>
-        <div className="relative left-12 top-44 h-32 w-64 font-['SF_Pro_Text'] text-[#560F0F] text-5xl font-bold leading-[60.46px] tracking-[-0.54px]">
+        <div className="relative left-12 top-44 h-32 w-64 font-['SF_Pro_Text'] text-5xl font-bold leading-[60.46px] tracking-[-0.54px] text-[#560F0F]">
           Prepping your reps!
         </div>
       </div>
 
       <div className="relative left-36 top-64">
-        <img src={'/assets/red-logo.svg'} alt='red-logo'/>
+        <img src={'/assets/red-logo.svg'} alt="red-logo" />
       </div>
     </div>
   );
