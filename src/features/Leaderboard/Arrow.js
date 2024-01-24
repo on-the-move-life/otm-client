d9794c3ff3846a1d0df5449dd3a4484b72bcc737
puspak <puspak9208@gmail.com>
2024-01-24 22:00:15 +0530

    ready for testing

--- src/features/Leaderboard/Arrow.js
+++ src/features/Leaderboard/Arrow.js
@@ -2,29 +2,28 @@ import React from 'react';
 
 const Arrow = ({ value }) => {
   const isPositive = value > 0;
+  const arrowImage =
+    value !== 0 ? (
+      <img
+        src={isPositive ? '/assets/upArrow.svg' : '/assets/downArrow.svg'}
+        alt={isPositive ? 'Up Arrow' : 'Down Arrow'}
+      />
+    ) : (
+      <div className="bg-white-300 h-7 w-4" />
+    );
+  const arrowText = value !== 0 ? (isPositive ? `+${value}` : `${value}`) : '';
 
   return (
-    <>
-      <div className="inline-flex h-7 w-4 flex-col items-center justify-start">
-        <div className="relative h-7 w-4 origin-top-left">
-          {value !== 0 ? (
-            <img
-              src={isPositive ? '/assets/upArrow.svg' : '/assets/downArrow.svg'}
-              alt={isPositive ? 'Up Arrow' : 'Down Arrow'}
-            />
-          ) : (
-            <div className="bg-white-300 h-7 w-4" />
-          )}
-        </div>
-        <div
-          className={`text-${
-            isPositive ? 'rose' : 'green'
-          }-500 text-sm font-bold lowercase tracking-tight`}
-        >
-          {value !== 0 ? (isPositive ? `+${value}` : `${value}`) : ''}
-        </div>
+    <div className="mr-2 mr-3 inline-flex h-7 w-4 flex-col items-center justify-start">
+      <div className="relative origin-top-left">{arrowImage}</div>
+      <div
+        className={`text-${
+          isPositive ? 'rose' : 'green'
+        }-500 text-sm font-bold lowercase tracking-tight`}
+      >
+        {arrowText}
       </div>
-    </>
+    </div>
   );
 };
 
--- src/features/Leaderboard/ListItem.js
+++ src/features/Leaderboard/ListItem.js
@@ -5,25 +5,33 @@ const ListItem = ({ imgUrl, isCurrentUser, user, mode }) => {
   const { rank, name, code, rankChange } = user;
   const count = mode === 'workout' ? user.workout : user.totalScore;
 
-  const defaultClassName = ` h-70 mix-blend-screen bg-neutral-700 bg-opacity-10 ${
-    isCurrentUser ? 'border border-purple-300 border-opacity-80 rounded-xl' : ''
-  } flex flex-row justify-between p-3`;
+  const defaultClassName = ` h-70  ${
+    isCurrentUser
+      ? 'mix-blend-screen bg-[#3C3C3C] bg-opacity-10 border border-purple-300 border-opacity-80 rounded-xl'
+      : ''
+  } flex flex-row justify-between py-3 px-auto`;
 
   return (
     <>
       <div className={defaultClassName}>
-        <div className="flex flex-row items-center justify-between px-3 ">
-          <span>{rank}</span>
-          <img
-            className="mx-4 h-8 rounded-full bg-blue"
-            src={imgUrl}
-            alt={name}
-          />
-          <span>{name}</span>
+        <div className="flex flex-row items-center justify-between px-3">
+          <span className="flex w-5 flex-row items-center justify-center leaderboard-gradient-text">
+            {rank}
+          </span>
+          <div className="flex flex-row items-center justify-center">
+            <img
+              className="mx-4 h-8 rounded-full bg-blue"
+              src={imgUrl}
+              alt={name}
+            />
+          </div>
+          <span className="text-lg font-normal">{name}</span>
         </div>
 
-        <div className="flex flex-row items-center justify-around px-4 ">
-          <span className="mr-4">{count}</span>
+        <div className="flex flex-row items-center justify-around px-4">
+          <span className="mr-4 flex w-5 flex-row items-center justify-center text-[21.47px] font-bold">
+            {count}
+          </span>
           <Arrow value={rankChange} />
         </div>
       </div>
