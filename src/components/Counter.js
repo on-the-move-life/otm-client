7e5f52d35ed29b6e0f48acf78543b0e592636441
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-01-29 23:28:48 +0530

    [refactor] : Counter.js - removed localstorage implementation for counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,36 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
-const Counter = () => {
+const Counter = ({ currentValue }) => {
   const [animationDuration, setAnimationDuration] = useState('10s');
   const [digits, setDigits] = useState([]);
 
   useEffect(() => {
-    let totalWorkouts = localStorage.getItem('workouts');
-    if (totalWorkouts && !totalWorkouts.includes('undefined')) {
-      totalWorkouts = JSON.parse(totalWorkouts);
-      console.log(totalWorkouts, 'total workouts');
-    }
-
-    const numberOfDigits = totalWorkouts.toString().length;
+    const numberOfDigits = currentValue.toString().length;
     setAnimationDuration(`${numberOfDigits * 10}s`);
-    getCounterDigits(totalWorkouts);
+    getCounterDigits(currentValue);
   }, []);
 
   const getCounterDigits = (value) => {
-    const incrementedValue = String(Number(value) + 1);
+    const incrementedValue = String(Number(value) + 1)
 
     // splitting the number into array of it's digits
-    const inputDigitsValue = value.toString().split('').map(Number);
-    const incrementedDigitsValue = incrementedValue
-      .toString()
-      .split('')
-      .map(Number);
+    const inputDigitsValue = value.toString().split('').map(Number)
+    const incrementedDigitsValue = incrementedValue.toString().split('').map(Number);
 
     // single digit number prefixed with 0
-    if (inputDigitsValue.length === 1) {
+    if(inputDigitsValue.length === 1){
       inputDigitsValue.unshift(0);
     }
-    if (incrementedDigitsValue.length === 1) {
+    if(incrementedDigitsValue.length === 1){
       incrementedDigitsValue.unshift(0);
     }
 
@@ -41,30 +32,33 @@ const Counter = () => {
     // creating a mapping between the original and incremented values
     incrementedDigitsValue.map((value, index) => {
       if (index === inputDigitsValue.length) {
-        setDigits((prev) => [...prev, ['', incrementedDigitsValue[index]]]);
-      } else {
-        if (inputDigitsValue[index] === incrementedDigitsValue[index]) {
-          setDigits((prev) => [...prev, [inputDigitsValue[index]]]);
-        } else
-          setDigits((prev) => [
-            ...prev,
-            [inputDigitsValue[index], incrementedDigitsValue[index]],
-          ]);
+        setDigits(prev => [...prev, ['', incrementedDigitsValue[index]]])
       }
-    });
+      else {
+        if(inputDigitsValue[index] === incrementedDigitsValue[index]){
+          setDigits(prev => [...prev, [inputDigitsValue[index]]])
+        }
+        else
+          setDigits(prev => [...prev, [inputDigitsValue[index], incrementedDigitsValue[index]]])
+      }
+    })
   };
 
   const renderCounterBoxes = () => {
     return digits.reverse().map((digit, index) => (
       <div key={index} className="countdown__box">
         <div className="countdown__inner">
-          {digit.map((value, i) => {
-            return <span key={index + '' + i}>{value}</span>;
-          })}
+          {
+            digit.map((value, i) => {
+              return <span key={index + '' + i}>{value}</span>
+            })
+          }
         </div>
       </div>
     ));
   };
+
+
 
   return (
     <>
