b660a2c9794be4cd543a106d7be6816696e30124
puspak <puspak9208@gmail.com>
2024-02-01 17:56:31 +0530

    connect to leaderboard and profile from home

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -4,7 +4,8 @@ import { Loader, Error } from '../../components';
 import { WeeklyWorkoutReport, FitnessScore, LeaderBoard, DuePaymentIndicator, MoveCoins } from '.';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { AiOutlinePoweroff, AiOutlineRight } from 'react-icons/ai';
+import { CgProfile } from "react-icons/cg";
+import { AiOutlineRight } from 'react-icons/ai';
 import { useUserContext } from '../../contexts/UserContext';
 
 const Home = () => {
@@ -22,9 +23,11 @@ const Home = () => {
 
   const navigate = useNavigate();
 
-  function handleLogout() {
-    navigate('/login', { replace: true });
-    logout();
+  function navigateToLeaderboard() {
+    navigate('/leaderboard', { replace: true });
+  }
+  function navigateToProfile() {
+    navigate('/profile', { replace: true });
   }
 
   useEffect(() => {
@@ -77,8 +80,8 @@ const Home = () => {
               <h1 className="inline-block bg-gradient-to-r from-[#9BF2C0] to-[#91BDF6]  bg-clip-text text-3xl font-semibold text-transparent">
                 {homeStats.name}
               </h1>
-              <button onClick={handleLogout}>
-                <AiOutlinePoweroff size={22} />
+              <button onClick={navigateToProfile}>
+                <CgProfile size={25} />
               </button>
             </div>
             <div className="flex items-center">
@@ -114,7 +117,7 @@ const Home = () => {
             <FitnessScore score={homeStats?.score} percentile={homeStats?.fitnessPercentileScore} />
           </section>
           <section className='w-full flex flex-row justify-center items-center gap-3'>
-            <LeaderBoard rank={homeStats?.rank} />
+            <LeaderBoard rank={homeStats?.rank} onClick= {navigateToLeaderboard}/>
             <MoveCoins coins={homeStats?.points}/>
           </section>
           {
