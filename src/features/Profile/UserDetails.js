cd42e8c0acbb7da449f5c403a13334afae7aa472
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-02-06 19:36:47 +0530

    [feat] : profile image update feature - backend integration not done

--- src/features/Profile/UserDetails.js
+++ src/features/Profile/UserDetails.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import styled from 'styled-components';
 import { useAuth } from '../../contexts/AuthContext';
 import { Loader } from '../../components';
 import { useNavigate } from 'react-router-dom';
@@ -6,10 +7,39 @@ import { formatDate } from '../../utils';
 import { axiosClient } from './apiClient';
 import { FaUserCircle } from 'react-icons/fa';
 import { HiArrowNarrowLeft } from 'react-icons/hi';
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { BsImageFill } from "react-icons/bs";
+import { IoMdTrash } from "react-icons/io";
+import { IoCamera } from "react-icons/io5";
+import { current } from '@reduxjs/toolkit';
+
+const ProfilePicHeading = styled.div`
+color: #D7D7D7;
+text-shadow: 0px 2.725px 2.725px rgba(0, 0, 0, 0.15);
+font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+font-size: 20px;
+font-style: normal;
+line-height: 29.066px; /* 160% */
+text-transform: capitalize;
+letter-spacing: 1px;
+`
+const IconLabel = styled.div`
+color: #D7D7D7;
+text-shadow: 0px 2.725px 2.725px rgba(0, 0, 0, 0.15);
+font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+font-size: 15px;
+font-style: normal;
+line-height: 29.066px; /* 160% */
+text-transform: capitalize;
+letter-spacing: 1px;
+`
 
 const UserDetails = ({ showHistory }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [memberData, setMemberData] = useState();
+  const profilePicRef = useRef(null);
+  const [showProfilePicPopup, setShowProfilePicPopup] = useState(false);
+  const [currentPic, setCurrentPic] = useState(null);
 
   const navigate = useNavigate();
 
@@ -48,8 +78,51 @@ const UserDetails = ({ showHistory }) => {
     return <Loader />;
   }
 
+  function handlePicChange(e) {
+    const file = e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setCurrentPic(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+
+    // make the api call to update the profile pic
+  }
+
   return (
     <>
+      {/* profile pic update popup */}
+      {showProfilePicPopup && <div className='w-full h-[200px] rounded-t-[30px] bg-gradient-to-r from-gray-500/30 to-gray-900/60 backdrop-blur-lg fixed bottom-0 left-0 z-50 p-5'>
+        <div className='absolute top-0 left-[47%] cursor-pointer' onClick={() => {
+          setShowProfilePicPopup(false);
+        }}>
+          <MdOutlineKeyboardArrowDown size={30} color='#D7D7D7' />
+        </div>
+        <div className='w-full flex flex-col items-start justify-around h-full mt-3 '>
+          <ProfilePicHeading>Profile photo</ProfilePicHeading>
+          <div className='w-full flex flex-row justify-start gap-[40px] items-ceter'>
+            <div className='w-fit flex flex-col justify-center items-center gap-1' onClick={() => {
+              profilePicRef.current.click();
+            }}>
+              <div className='border-gray-500 border-[0.5px] rounded-full p-3 cursor-pointer'>
+                <BsImageFill size={30} color='#5ECC7B' />
+              </div>
+              <IconLabel>Gallery</IconLabel>
+            </div>
+            <div className='w-fit flex flex-col justify-center items-center gap-1' onClick={() => {
+              setCurrentPic(null)
+            }}>
+              <div className='border-gray-500 border-[0.5px] rounded-full p-3 cursor-pointer'>
+                <IoMdTrash size={30} color='gray' />
+              </div>
+              <IconLabel>Delete</IconLabel>
+            </div>
+          </div>
+        </div>
+      </div>}
       {memberData && (
         <div className="h-screen w-screen overflow-x-auto px-4 pb-32 pt-8">
           <div className="mb-4">
@@ -63,9 +136,19 @@ const UserDetails = ({ showHistory }) => {
           <h1 className="inline-block bg-gradient-to-r from-[#9BF2C0] to-[#91BDF6] bg-clip-text text-3xl font-semibold text-transparent">
             My Profile
           </h1>
+
+          {/* User Profile Pic and Name */}
           <div className="flex flex-col items-center justify-center">
             <div className="mt-6 flex flex-col items-center justify-center">
-              <FaUserCircle size={40} color={'#91BDF6'} />
+              <div className='w-[100px] h-[100px] rounded-full relative'>
+                {currentPic ? <img src={currentPic} alt="user Profile pic" className='w-[100px] h-[100px] rounded-full' /> : <FaUserCircle size={100} color={'#91BDF6'} />}
+                <div className='w-[40px] h-[40px] flex flex-row justify-center items-center rounded-full bg-green absolute bottom-0 right-0' onClick={() => {
+                  setShowProfilePicPopup(true);
+                }}>
+                  <IoCamera size={25} color="black" />
+                </div>
+                <input ref={profilePicRef} type='file' accept='image/png, image/jpg, image/jpeg' name="profile image" hidden onChange={handlePicChange}></input>
+              </div>
               <div className="text-neutral-400 text-xl font-medium capitalize leading-loose">
                 {memberData.name}
               </div>
