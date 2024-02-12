007b345974822c294b72c401e689803bb1a1282b
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-02-12 13:19:15 +0530

    [feat] : profile picture feature - delete feature remaining

--- src/features/Profile/UserDetails.js
+++ src/features/Profile/UserDetails.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import styled from 'styled-components';
 import { useAuth } from '../../contexts/AuthContext';
 import { Loader } from '../../components';
 import { useNavigate } from 'react-router-dom';
@@ -6,10 +7,44 @@ import { formatDate } from '../../utils';
 import { axiosClient } from './apiClient';
 import { FaUserCircle } from 'react-icons/fa';
 import { HiArrowNarrowLeft } from 'react-icons/hi';
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { BsImageFill } from "react-icons/bs";
+import { IoMdTrash } from "react-icons/io";
+import { IoCamera } from "react-icons/io5";
+import axios from 'axios';
+
+const ProfilePicHeading = styled.div`
+color: #D7D7D7;
+text-shadow: 0px 2.725px 2.725px rgba(0, 0, 0, 0.15);
+font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+font-size: 20px;
+font-style: normal;
+line-height: 29.066px; /* 160% */
+text-transform: capitalize;
+letter-spacing: 1px;
+`
+const IconLabel = styled.div`
+color: #D7D7D7;
+text-shadow: 0px 2.725px 2.725px rgba(0, 0, 0, 0.15);
+font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+font-size: 15px;
+font-style: normal;
+line-height: 29.066px; /* 160% */
+text-transform: capitalize;
+letter-spacing: 1px;
+`
 
 const UserDetails = ({ showHistory }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [memberData, setMemberData] = useState();
+  const profilePicRef = useRef(null);
+  const profilePicCameraRef = useRef(null);
+  const [showProfilePicPopup, setShowProfilePicPopup] = useState(false);
+  // state to store the chosen profile pic
+  const [chosenPic, setChosenPic] = useState(null);
+  // state to store the file object to send to the server
+  const [profilePicFile, setProfilePicFile] = useState(null);
+  const [uniqueImageURLKey, setUniqueImageURLKey] = useState(null);
 
   const navigate = useNavigate();
 
@@ -30,6 +65,9 @@ const UserDetails = ({ showHistory }) => {
 
       if (res.data) {
         const data = res.data;
+        // Trick to avoid the memory caching by the browser, so that the updated profile pic is displayed
+        const uniqueKey = Date.now();
+        setUniqueImageURLKey(`${data?.profilePicture}?key=${uniqueKey}`);
         setMemberData({ ...data, ...user });
       }
     } catch (error) {
@@ -48,8 +86,70 @@ const UserDetails = ({ showHistory }) => {
     return <Loader />;
   }
 
+  function handlePicChange(e) {
+    const file = e.target.files[0];
+    if (file) {
+      setProfilePicFile(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setChosenPic(reader.result);
+      };
+      reader.readAsDataURL(file);
+      e.target.value = null;
+      setShowProfilePicPopup(false);
+      const formData = new FormData();
+      formData.append('profilePicture', file);
+      formData.append('email', JSON.parse(localStorage.getItem('user')).email);
+      axios
+        .post(`${process.env.REACT_APP_INSIGHT_SERVICE_BASE_URL}/client/profile-picture`, formData)
+        .then(res => {
+          console.log(res)
+        })
+    }
+
+  }
+
   return (
     <>
+      {/* profile pic update popup */}
+      {showProfilePicPopup && <div className='w-full h-[200px] rounded-t-[30px] bg-gradient-to-r from-gray-500/30 to-gray-900/60 backdrop-blur-lg fixed bottom-0 left-0 z-50 p-5'>
+        <button className='absolute top-0 left-[47%] cursor-pointer' onClick={() => {
+          setShowProfilePicPopup(false);
+        }}>
+          <MdOutlineKeyboardArrowDown size={30} color='#D7D7D7' />
+        </button>
+        <div className='w-full flex flex-col items-start justify-around h-full mt-3 '>
+          <ProfilePicHeading>Profile photo</ProfilePicHeading>
+          <div className='w-full flex flex-row justify-start gap-[40px] items-ceter'>
+            <div className='w-fit flex flex-col justify-center items-center gap-1' onClick={() => {
+              profilePicCameraRef.current.click();
+            }}>
+              <button className='border-gray-500 border-[0.5px] rounded-full p-3 cursor-pointer'>
+                <IoCamera size={30} color='#5ECC7B' />
+              </button>
+              <IconLabel>Camera</IconLabel>
+            </div>
+            <div className='w-fit flex flex-col justify-center items-center gap-1' onClick={() => {
+              profilePicRef.current.click();
+            }}>
+              <button className='border-gray-500 border-[0.5px] rounded-full p-3 cursor-pointer'>
+                <BsImageFill size={30} color='#5ECC7B' />
+              </button>
+              <IconLabel>Gallery</IconLabel>
+            </div>
+            <div className='w-fit flex flex-col justify-center items-center gap-1' onClick={() => {
+              setChosenPic(null);
+              setProfilePicFile(null); // reset the file object
+              setShowProfilePicPopup(false); // close the popup after deleting the pic
+            }}>
+              <button className='border-gray-500 border-[0.5px] rounded-full p-3 cursor-pointer'>
+                <IoMdTrash size={30} color='gray' />
+              </button>
+              <IconLabel>Delete</IconLabel>
+            </div>
+          </div>
+        </div>
+      </div>}
       {memberData && (
         <div className="h-screen w-screen overflow-x-auto px-4 pb-32 pt-8">
           <div className="mb-4">
@@ -63,9 +163,24 @@ const UserDetails = ({ showHistory }) => {
           <h1 className="inline-block bg-gradient-to-r from-[#9BF2C0] to-[#91BDF6] bg-clip-text text-3xl font-semibold text-transparent">
             My Profile
           </h1>
+
+          {/* User Profile Pic and Name */}
           <div className="flex flex-col items-center justify-center">
-            <div className="mt-6 flex flex-col items-center justify-center">
-              <FaUserCircle size={40} color={'#91BDF6'} />
+            <div className="mt-6 flex flex-col items-center justify-center gap-1">
+              <div className='w-[100px] h-[100px] rounded-full relative'>
+                {chosenPic ?
+                  <img src={chosenPic} alt="user Profile pic" className='w-[100px] h-[100px] rounded-full object-cover' /> :
+                  memberData && memberData?.profilePicture ?
+                    <img src={uniqueImageURLKey} alt="user Profile pic" className='w-[100px] h-[100px] rounded-full object-cover' /> :
+                    <FaUserCircle size={100} color={'#91BDF6'} />}
+                <button className='w-[40px] h-[40px] flex flex-row justify-center items-center rounded-full bg-green absolute bottom-0 right-0' onClick={() => {
+                  setShowProfilePicPopup(true);
+                }}>
+                  <IoCamera size={25} color="black" />
+                </button>
+                <input ref={profilePicRef} type='file' accept='image/png, image/jpg, image/jpeg' name="profile image" hidden onInput={handlePicChange}></input>
+                <input ref={profilePicCameraRef} type='file' capture="user" accept='image/png, image/jpg, image/jpeg' name="profile image camera" hidden onInput={handlePicChange}></input>
+              </div>
               <div className="text-neutral-400 text-xl font-medium capitalize leading-loose">
                 {memberData.name}
               </div>
@@ -132,14 +247,14 @@ const UserDetails = ({ showHistory }) => {
                 </section>
               </div>
             </div>
-            <div className="flex flex-col pt-52" onClick={handleLogout}>
+            <div className="flex flex-col pt-52">
               <div className="bg-neutral-700 border-zinc-400 mx-auto inline-flex h-12 w-[358px] items-center justify-center gap-2.5 rounded-lg border bg-opacity-5 p-2.5">
                 <div className="relative h-5 w-5 origin-top-left">
                   <img src="./assets/logout.svg" alt="" />
                 </div>
-                <div className="text-lg font-medium text-lightGray">
+                <button className="text-lg font-medium text-lightGray" onClick={handleLogout}>
                   Log Out
-                </div>
+                </button>
               </div>
             </div>
           </div>
