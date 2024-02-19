f4f9519a3fa2fb16dfe1f16bd7c8a5fae638c859
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-02-19 19:16:05 +0530

    Profile Picture added to the leaderboards

--- src/features/Leaderboard/ListItem.js
+++ src/features/Leaderboard/ListItem.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import Arrow from './Arrow';
+import { FaUserCircle } from 'react-icons/fa';
 
 const ListItem = ({ isCurrentUser, user, mode }) => {
-  const { rank, name, code, rankChange } = user;
+  const { rank, name, code, rankChange, profilePicture } = user;
   const count = mode === 'workout' ? user.workout : user.totalScore;
 
-  const defaultClassName = ` h-[75px]  ${
-    isCurrentUser
+  const defaultClassName = ` h-[75px]  ${isCurrentUser
       ? 'mix-blend-screen bg-[#3C3C3C] bg-opacity-10 leaderboard-user-list-item border-opacity-80 rounded-xl'
       : ''
-  } w-full flex flex-row justify-between px-auto`;
+    } w-full flex flex-row justify-between px-auto`;
 
   return (
     <div className='w-full border-b-[0.5px] border-gray-600'>
       <div className={defaultClassName}>
-        <div className="w-full flex flex-row items-center justify-start px-3">
+        <div className="w-full flex flex-row items-center justify-start px-3 gap-2">
           <div className="basis-1/6 flex flex-row items-center justify-start leaderboard-gradient-text">
             {rank}
           </div>
-          {/* <div className="flex flex-row items-center justify-center">
-            <img
-              className="mx-4 h-8 rounded-full bg-blue"
-              src="/assets/lb-ph.png"
-              alt={name}
-            />
-          </div> */}
+          {
+            profilePicture !== '' ? <div className="flex flex-row items-center justify-center">
+              <img
+                className="mx-4 h-[40px] w-[40px] rounded-full bg-blue"
+                src="/assets/lb-ph.png"
+                alt={name}
+              />
+            </div> :
+            <FaUserCircle size={40} color={'#91BDF6'} />
+          }
           <div className="basis-5/6 text-lg font-normal">{name}</div>
         </div>
 
@@ -36,7 +39,7 @@ const ListItem = ({ isCurrentUser, user, mode }) => {
         </div>
       </div>
       {!isCurrentUser && <div className="w-7/8 h-[0.5] bg-gray-700"></div>}
-      
+
     </div>
   );
 };
