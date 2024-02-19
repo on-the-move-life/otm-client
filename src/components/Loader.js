02b09aca5f6be00d3466b4b672d0a665aad3d55a
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-02-19 20:22:10 +0530

    [refactor] : Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,11 +3,7 @@ import React from 'react';
 const Loader = ({ className }) => {
   console.log('className', className);
   return (
-    className !== undefined ?
-      <div className={"text-green flex w-screen items-center justify-center text-2xl " + className}>
-        LOADING...
-      </div> :
-      <div className="text-green flex h-screen w-screen items-center justify-center text-2xl ">
+      <div className={`text-green flex w-screen items-center justify-center text-2xl ${className !== undefined ? className : ' h-screen'}`}>
         LOADING...
       </div>
   );
