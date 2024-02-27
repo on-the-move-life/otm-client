4254a4e0bbdb877a62e51c3ecefc350024cf31b4
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-02-28 00:02:26 +0530

    [style] : new loading page implemented

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Loader = ({ className }) => {
   return (
-      <div className={`text-green flex w-screen items-center justify-center text-2xl ${className !== undefined ? className : ' h-screen'}`}>
-        LOADING...
-      </div>
+    <div class={`loading w-full flex flex-row items-center justify-center ${className !== undefined ? className : 'h-screen'}`}>
+      <svg width="64px" height="48px">
+        <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="back"></polyline>
+        <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="front"></polyline>
+      </svg>
+    </div>
   );
 };
 
