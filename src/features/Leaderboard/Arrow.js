b21cecbe7cc906856c2100b03161da19601ba010
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-02-28 11:53:23 +0530

    [refactor] : Arrow.js - abstraction : user need not to specify if he wants decimal value or not - output is based on the input value.

--- src/features/Leaderboard/Arrow.js
+++ src/features/Leaderboard/Arrow.js
@@ -1,8 +1,9 @@
-import { toInteger } from 'lodash';
+import { isInteger, toInteger } from 'lodash';
 import React from 'react';
 
-const Arrow = ({ value, showDecimalValue=true }) => {
+const Arrow = ({ value }) => {
   const isPositive = value > 0;
+  const integer = isInteger(value);
   const arrowImage =
     value !== 0 ? (
       <img
@@ -12,7 +13,7 @@ const Arrow = ({ value, showDecimalValue=true }) => {
     ) : (
       <div className="bg-white-300 h-7 w-4" />
     );
-  const arrowText = value !== 0 ? (isPositive ? `+${showDecimalValue ? value?.toFixed(1) : toInteger(value)}` : `${showDecimalValue ? value?.toFixed(1) : toInteger(value)}`) : '';
+  const arrowText = value !== 0 ? (isPositive ? `+${!integer ? value?.toFixed(1) : toInteger(value)}` : `${!integer ? value?.toFixed(1) : toInteger(value)}`) : '';
 
   return (
     <div className="mr-2 inline-flex h-7 w-4 flex-col items-center justify-start">
