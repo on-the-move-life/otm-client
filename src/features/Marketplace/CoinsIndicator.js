3473881d08b20aa8193c87e6227a15420029a829
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-03-18 18:03:04 +0530

    [refactor] : coinIndicator functions improved

--- src/features/Marketplace/CoinsIndicator.js
+++ src/features/Marketplace/CoinsIndicator.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 function CoinsIndicator({ coins, offers }) {
     const deviceWidth = window.innerWidth < 400 ? window.innerWidth - 50 : 350;
     const [lastRange, setLastRange] = useState(0);
+    const [startRange, setStartRange] = useState(0);
     const [coinLength, setCoinLength] = useState(0);
     const [offerLength, setOfferLength] = useState(0);
     const [roundedOfferLength, setRoundedOfferLength] = useState(0);
@@ -12,39 +13,46 @@ function CoinsIndicator({ coins, offers }) {
         if(value >= 1000){
             return `${value/1000}k`
         }
+        else if(value > 0 && value < 1000){
+            return `${(value/1000).toFixed(1)}k`
+        }
+        else{
+            return '0k'
+        }
     }
 
-    useEffect(() => {
-        let maxOffer = 100000;
-        offers && offers.map((offer) => {
-            if (offer?.requiredMovecoins - coins >= 0 && offer?.requiredMovecoins - coins <= maxOffer) {
-                console.log("hello")
-                maxOffer = offer.requiredMovecoins;
-                console.log("maxOffer: ", maxOffer);
-                setNearestOffer(offer.requiredMovecoins);
+    function calculateIndicatorValues(range=1000, totalCoins=2100, offersArray=offers){
+        // range of the bar [lowerLimit, upperLimit] such that upperLimit - lowerLimit = range
+        const lowerLimit = Math.floor(totalCoins/range) * range;
+        const upperLimit = Math.ceil(totalCoins/range) * range;
+        // length of the moveCoin bar calculated as per the total coins
+        const moveCoinBarLength = ((totalCoins - lowerLimit) / range) * deviceWidth;
+
+        // calculation of the nearest offer as per the movecoins user has
+        let maxOffer = Infinity;
+        offersArray && offersArray.map((offer) => {
+            if (offer?.requiredMovecoins - totalCoins >= 0 && offer?.requiredMovecoins - totalCoins <= maxOffer && offer?.requiredMovecoins <= upperLimit && offer?.requiredMovecoins >= lowerLimit) {
+                maxOffer = offer?.requiredMovecoins;
+                setNearestOffer(maxOffer);
             }
-        });
+        })
 
-        if (coins > 0) {
-            const ceilingMaxOffer = Math.ceil(nearestOffer / 2000) * 2000;
-            const ceilingCoins = Math.ceil(coins / 2000) * 2000;
-            const range = (ceilingMaxOffer > ceilingCoins) ? ceilingMaxOffer : ceilingCoins;
-            setLastRange(range);
-            const lengthOfCoin = (coins / range) * deviceWidth;
-            setCoinLength(lengthOfCoin);
+        // setting the values to their respective states
+        setLastRange(upperLimit);
+        setStartRange(lowerLimit);
+        setCoinLength(moveCoinBarLength);
 
-            const calculatedOfferLength = (nearestOffer / range) * deviceWidth;
+        if(nearestOffer > 0 && nearestOffer <= upperLimit && nearestOffer >= lowerLimit){
+            const calculatedOfferLength = ((nearestOffer - lowerLimit) / range) * deviceWidth;
             const roundedOfferLength = calculatedOfferLength === 0 ? 0 : Math.floor(calculatedOfferLength);
             setOfferLength(calculatedOfferLength);
+            // rounded offer length is needed to make an array of this size(integer) and to render the yellow bars
             setRoundedOfferLength(roundedOfferLength);
-        } else {
-            setLastRange(2000);
-            setCoinLength(0);
-            setOfferLength(0);
-            setRoundedOfferLength(0);
         }
-        console.log('nearest',nearestOffer)
-    }, [coins, offers, lastRange, roundedOfferLength, deviceWidth, nearestOffer]);
+    }
+    useEffect(() => {
+        calculateIndicatorValues();
+    }, [coins, offers]);
 
 
     return (
@@ -52,13 +60,13 @@ function CoinsIndicator({ coins, offers }) {
             <div className='flex flex-col items-center justify-center gap-1 relative' style={{ width: deviceWidth }}>
                 <div className='h-[12px] rounded-[24px] bg-gradient-to-r from-blue-gray-800/30 to to-gray-800/100 relative' style={{ width: deviceWidth }}>
                     <div className={`bg-green rounded-[24px] h-full absolute flex flex-row items-center z-[10]`} style={{ width: coinLength }}>
-                        {/* {coinLength > 0 && <div className='bg-white rounded-full w-[8px] h-[8px] absolute right-[4px]'></div>} */}
+                        {coinLength > 0 && <div className='bg-white rounded-full w-[8px] h-[8px] absolute right-[2px]'></div>}
                     </div>
                     {nearestOffer > 0 && nearestOffer <= lastRange &&
                         <div className={`rounded-[24px] h-full absolute flex flex-row items-center z-[5]`} style={{ width: offerLength }}>
                             {roundedOfferLength !== 0 && [...Array(Number(roundedOfferLength))].map((_, index) => {
                                 if (index % 5 === 0 && index !== 0 && index <= roundedOfferLength - 4) {
-                                    return <div key={index} className='bg-yellow rounded-full w-[2px] h-[12px] absolute rounded-t-sm rounded-b-sm' style={{ left: index }}></div>;
+                                    return <div key={index} className='bg-yellow rounded-full w-[2px] h-[10px] absolute rounded-t-sm rounded-b-sm' style={{ left: index }}></div>;
                                 }
                                 return null; // Default return value
                             })}
@@ -68,9 +76,10 @@ function CoinsIndicator({ coins, offers }) {
                         </div>
                     }
                 </div>
-                <div className='text-[12px] text-[#545454] absolute left-0 top-[100%]'>0</div>
-                <div className='text-[12px] text-[#545454] absolute right-0 top-[100%]'>{formatThousandValues(lastRange)}</div>
+                <div className='text-[12px] text-[#545454] absolute left-0 top-[-20px]'>{formatThousandValues(startRange)}</div>
+                <div className='text-[12px] text-[#545454] absolute right-0 top-[-20px]'>{formatThousandValues(lastRange)}</div>
                 <div className={`text-[12px] text-[#545454] absolute top-[100%]`} style={{ left: coinLength - 15 }}>{formatThousandValues(coins)}</div>
+                <div className={`text-[12px] text-[#545454] absolute top-[100%]`} style={{ left: offerLength - 15 }}>{formatThousandValues(nearestOffer)}</div>
                 {nearestOffer > 0 && nearestOffer <= lastRange &&
                     <div className={`w-[100px] h-[50px] text-[12px] text-black font-bold absolute top-[-35px] bg-no-repeat bg-cover flex flex-row justify-center items-start`} style={{ left: offerLength - 58, backgroundImage: `url(${'/assets/offer_dialogue.svg'})` }}>
                         <div className='pt-1'>New Offer</div>
