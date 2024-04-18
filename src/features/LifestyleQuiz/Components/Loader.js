43cd00c0963e085fd380493a9d5c3fdae4754285
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-04-18 11:51:16 +0530

    Loader issue resolved : lifestyle loader was being displayed throughout the app

--- src/features/LifestyleQuiz/Components/Loader.js
+++ src/features/LifestyleQuiz/Components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Loader.css";
 function Loader({ className, message }) {
     return (
-        <div class={`loading w-full flex flex-col items-center justify-center gap-[7rem] ${className !== undefined ? className : 'h-screen'}`}>
+        <div class={`loading-lifestyle w-full flex flex-col items-center justify-center gap-[7rem] ${className !== undefined ? className : 'h-screen'}`}>
             <svg width="64px" height="48px">
                 <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="back"></polyline>
                 <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="front"></polyline>
