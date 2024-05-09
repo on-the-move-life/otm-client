c05a57e32781ec5e4629e7bdaeabe86181d29d8f
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-05-09 11:04:06 +0530

    Bug Fix for iOS Safari

--- src/features/Questionnaire/BMIScreen.js
+++ src/features/Questionnaire/BMIScreen.js
@@ -5,7 +5,7 @@ import BackButton from '../../components/BackButton';
 import { ProgressBar } from '../LifestyleQuiz';
 function BMIScreen({ screen, questions, response, getScreenCounts, decreaseScreenAndRank, setScreen, submitResponse, setShowBMIScreen }) {
     return (
-        <div className='fixed min-h-screen w-full flex flex-col justify-between items-center px-6 py-9 top-0 left-0 z-50 bg-black'>
+        <div className='absolute min-h-screen w-full flex flex-col justify-between items-center px-6 py-9 top-0 left-0 z-50 bg-black'>
             <div className="flex flex-col items-center justify-center gap-5">
                 <div className="mx-auto my-4 flex w-full items-center justify-center">
                     <BackButton
@@ -32,13 +32,15 @@ function BMIScreen({ screen, questions, response, getScreenCounts, decreaseScree
                     <ScoreIndicator height={Number(response['su4'][0])} weight={Number(response['su3'][0])} />
                 </div>
             </div>
-            <Button
-                text={"Next"}
-                type="lifestyle"
-                action={() => {
-                    setShowBMIScreen(false);
-                }}
-            />
+            <div className="w-full mb-[44px]">
+                <Button
+                    text={"Next"}
+                    type="lifestyle"
+                    action={() => {
+                        setShowBMIScreen(false);
+                    }}
+                />
+            </div>
         </div>
     )
 }
