82e75b595d77efd70ef717041cff785060f9439c
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-05-22 13:56:47 +0530

    Revert "Revert "Merge branch 'ON-183-routine-frontend' of https://github.com/on-the-move-life/otm-client into ON-183-routine-frontend""
    This reverts commit 0758e41163709297bcc413b4584d34c4e774c7aa.

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,10 @@ module.exports = withMT({
         blue: '#7E87EF',
         lightGray: '#B1B1B1',
         yellow: '#F5C563',
+        mediumGray: '#1C1C1E',
+        lightGray: '#929292',
+        custompurple: '#7E87EF',
+        customGray: '#545454',
       },
       backgroundImage: {
         'landing-cover': "url('../public/assets/landing-cover.png')",
@@ -31,6 +35,7 @@ module.exports = withMT({
       },
       fontFamily: {
         serif: ['Inter', 'sans-serif'],
+        sfpro: ['"SF Pro Display"', 'sans-serif'],
       },
     },
   },
