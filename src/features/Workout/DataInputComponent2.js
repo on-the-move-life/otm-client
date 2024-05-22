c69efbc4f0c109b150a1d4901d367a544d5b9f7d
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-05-22 13:54:58 +0530

    Revert "[feat] : interconversion of kg-lbs"
    This reverts commit ac72cd50117d4c54abfd64959b2367eca5e05930.

--- src/features/Workout/DataInputComponent2.js
+++ src/features/Workout/DataInputComponent2.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import useLocalStorage from '../../hooks/useLocalStorage';
 
 const DataInputComponent = ({
@@ -14,27 +14,6 @@ const DataInputComponent = ({
     const textInputRef = useRef('');
     const [storedValue, setValue, getItem] = useLocalStorage(inputId, '');
     const [storedInputValues, setStoredInputValues, getStoredInputValues] = useLocalStorage('inputIds', []);
-    const [isKg, setIsKg] = useState(true);
-    /**
-     * Converts weight to kilograms or pounds.
-     *
-     * @param {number} weight - The weight to be converted. Should be a positive number.
-     * @param {boolean} isKg - A flag indicating whether the input weight is in kilograms. If false, the weight is in pounds.
-     * @returns {number} - The weight in kilograms if isKg is true, or the weight in pounds rounded to two decimal places if isKg is false.
-     * @throws {Error} - Throws an error if the weight is not a positive number.
-    */
-    const toKg = (weight, isKg) => {
-        if (typeof weight !== 'number' || weight <= 0) {
-            throw new Error('Weight must be a positive number.');
-        }
-
-        if (isKg) {
-            return weight;
-        } else {
-            const weightInKg = weight / 2.20462;
-            return parseFloat(weightInKg.toFixed(2)); // Round to two decimal places
-        }
-    }
     const handleInputChange = (value) => {
         setValue(value);
 
@@ -47,25 +26,8 @@ const DataInputComponent = ({
             setStoredInputValues([inputId]);
         }
     };
-
-    /**
-     * Validates the input to accept only positive numbers and decimal values up to 2 decimal places.
-     * Removes any non-numeric characters except for one decimal point, and limits the number of decimal places to 2.
-     *
-     * @param {string} input - The input string to be validated and cleaned.
-     * @returns {string} - The cleaned input string containing only positive numbers and at most one decimal point with up to 2 decimal places.
-     */
-    const validateInput = (input) => {
-        // Remove any character that is not a digit or a decimal point
-        const cleanedInput = input.replace(/[^0-9.]/g, '');
-        // Ensure only one decimal point is allowed in the string
-        const singleDecimalInput = cleanedInput.replace(/(\..*?)\..*/g, '$1');
-        // Limit to 2 decimal places
-        const decimalIndex = singleDecimalInput.indexOf('.');
-        if (decimalIndex !== -1) {
-            return singleDecimalInput.slice(0, decimalIndex + 3); // Include up to 2 digits after the decimal point
-        }
-        return singleDecimalInput;
+    const validateInput = (number) => {
+        return number.replace(/[^0-9]/g, '');
     }
 
     const inputDropdownStyle = {
@@ -80,22 +42,7 @@ const DataInputComponent = ({
 
     // Set default value to an empty string if not provided
     const value = storedValue !== undefined ? storedValue : '';
-    const [weightValue, setWeightValue] = useState(value);
 
-    useEffect(() => {
-        try {
-            if(weightValue === ""){
-                handleInputChange("");
-            }
-            else{
-                const weigthInKg = toKg(Number(weightValue), isKg);
-                handleInputChange(weigthInKg.toString());
-            }
-        }
-        catch (e) {
-            console.log("error : ", e);
-        }
-    }, [weightValue, isKg])
 
     return (
         <div className="py-4">
@@ -142,31 +89,12 @@ const DataInputComponent = ({
                     </label>
                     <div className="w-full relative mt-2 text-gray-500">
                         <div className="absolute inset-y-0 left-3 my-auto h-6 flex items-center border-r pr-2">
-                            <select className="text-sm outline-none rounded-lg h-full bg-transparent" onChange={(e) => {
-                                if (e.target.value === "kg") {
-                                    setIsKg(true);
-                                    if (weightValue !== "") {
-                                        const lbsWeight = Number(weightValue);
-                                        const kgValue = (lbsWeight / 2.20462).toFixed(2);
-                                        setWeightValue(kgValue);
-                                    }
-                                }
-                                else {
-                                    setIsKg(false);
-                                    if (weightValue !== "") {
-                                        const kgWeight = Number(weightValue);
-                                        const lbsValue = (kgWeight * 2.20462).toFixed(2);
-                                        setWeightValue(lbsValue)
-                                    }
-                                }
-                            }}>
+                            <select className="text-sm outline-none rounded-lg h-full bg-transparent">
                                 <option>kg</option>
                                 <option>lbs</option>
                             </select>
                         </div>
-                        <input type="number" placeholder={placeholder} value={weightValue} pattern={"[0-9.+-e]"} className="textbox-kginput w-full pl-[4.5rem] pr-3 py-2 text-[#b1b1b1] appearance-none bg-transparent outline-none border focus:border-slate-600 shadow-sm rounded-lg" onChange={(e) => {
-                            setWeightValue(validateInput(e.target.value));
-                        }} />
+                        <input type="number" placeholder={placeholder} pattern={"[0-9.+-e]"} className="textbox-kginput w-full pl-[4.5rem] pr-3 py-2 text-[#b1b1b1] appearance-none bg-transparent outline-none border focus:border-slate-600 shadow-sm rounded-lg" onChange={(e) => handleInputChange(validateInput(e.target.value))}/>
                     </div>
                 </div>
             ) : (
