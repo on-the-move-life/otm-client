9cebafedd819c2ebbae22b4b7946fd91484b29d1
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-06-03 17:05:19 +0530

    Loader and Error handling

--- src/features/LifeStyleRoutines/MainPage.js
+++ src/features/LifeStyleRoutines/MainPage.js
@@ -7,6 +7,8 @@ import BackButton from '../../components/BackButton';
 import Routines from './Routines'
 import Summary from './Summary'
 import { useNavigate } from 'react-router-dom'
+import { Loader } from '../LifestyleQuiz'
+import { Error } from '../../components'
 
 function MainPage() {
   // Defining states for the fetched data
@@ -19,10 +21,13 @@ function MainPage() {
    * indices : 0 for Routine & 1 for Summary
    */
   const [section, setSection] = useState(0);
+  const [pageLoading, setPageLoading] = useState(false);
+  const [pageError, setPageError] = useState(false);
 
   const navigate = useNavigate();
 
   function getData(date) {
+    setPageLoading(true);
     console.log(date)
     const memberCode = JSON.parse(localStorage.getItem('user'))['code'];
     axiosClient.get(`?user=${memberCode}&date=${date}`)
@@ -31,34 +36,44 @@ function MainPage() {
         setCircles(res.data?.lifeStyle?.circles);
         setPercentCompletion(res.data?.lifeStyle?.completionPercentage);
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        console.log(err);
+        setPageError(true);
+      })
+      .finally(() => setPageLoading(false))
   }
 
   useEffect(() => {
     // initially when the selected date is null, call for today's date
-    if(selectedDate === null){
+    if (selectedDate === null) {
       const currentDate = getFormattedDate();
       getData(currentDate);
     }
-    else{
+    else {
       getData(selectedDate)
     }
   }, [selectedDate])
 
   return (
     <>
+      {pageLoading && (
+        <div className="fixed left-0 top-0 z-50 w-full bg-black">
+          <Loader className={'h-screen w-full'} />
+        </div>
+      )}
+      {pageError && !pageLoading && <div className='w-full h-screen fixed top-0 left-0 z-[200] bg-black'><Error>Some Error Occured</Error></div>}
       <BackButton
         size={30}
         className="absolute left-3 top-2 w-fit cursor-pointer"
         action={() => navigate('/')}
       />
       <div className="h-full px-3 py-5 flex flex-col justify-start items-center gap-3 mb-9 mt-7">
-        {completionHistory && <Calendar completionHistory={completionHistory} isSummaryPage={section === 1} selectedDate={selectedDate} setSelectedDate={setSelectedDate}/>}
+        {completionHistory && <Calendar completionHistory={completionHistory} isSummaryPage={section === 1} selectedDate={selectedDate} setSelectedDate={setSelectedDate} />}
         {
-          section === 0 && <Routines circles={circles} date={selectedDate}/>
+          section === 0 && <Routines circles={circles} date={selectedDate} />
         }
         {
-          section === 1 && <Summary circles={circles} date={selectedDate}/>
+          section === 1 && <Summary circles={circles} date={selectedDate} />
         }
       </div>
       <div className='w-full fixed bottom-0 left-0 py-4 bg-black/20 backdrop-blur-sm'>
