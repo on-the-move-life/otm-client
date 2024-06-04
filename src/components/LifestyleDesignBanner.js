006e2178391f0f454ac4ea4ec3f7b3c63fec4140
vrindatyagi <vrindatyagi09@gmail.com>
2024-06-04 12:27:02 +0530

    removed a full stop

--- src/components/LifestyleDesignBanner.js
+++ src/components/LifestyleDesignBanner.js
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const LifestyleDesignBanner = () => {
-    return (
-        <div className='w-auto p-1 mb-2 flex items-center justify-between rounded-md bg-gradient-to-b from-gradientStart to-gradientEnd'>
+  return (
+    <div className="mb-2 flex w-auto items-center justify-between rounded-md bg-gradient-to-b from-gradientStart to-gradientEnd p-1">
+      {/* elem 1 */}
+      <img src="/assets/ldbannerleft.svg" alt="lifestyle design banner" />
 
-            {/* elem 1 */}
-            <img src="/assets/ldbannerleft.svg" alt="lifestyle design banner" />
+      <div className="flex font-sfpro  text-sm font-semibold leading-none text-black">
+        <img
+          className="mr-1"
+          src="/assets/ldbannertime.svg"
+          alt="lifestyle design banner"
+        />
+        Routine Tracking Coming Soon!
+      </div>
 
-            <div className='text-black flex  font-sfpro text-sm font-semibold leading-none'>
-                <img className='mr-1' src="/assets/ldbannertime.svg" alt="lifestyle design banner" />
-                Routine Tracking. Coming Soon!
-            </div>
+      {/* elem 4 */}
+      <img src="/assets/ldbannerright.svg" alt="lifestyle design banner" />
+    </div>
+  );
+};
 
-            {/* elem 4 */}
-            <img src="/assets/ldbannerright.svg" alt="lifestyle design banner" />
-
-
-
-
-        </div>
-    )
-}
-
-export default LifestyleDesignBanner
+export default LifestyleDesignBanner;
