2bc2d75fe16253609858c2362d3fc73e35cd7456
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-06-10 11:40:48 +0530

    time added on mainpage circles

--- src/features/LifeStyleRoutines/Routines.js
+++ src/features/LifeStyleRoutines/Routines.js
@@ -8,7 +8,7 @@ function Routines({ circles, date, setReloadCounter }) {
       {
         circles && circles.map(circle => {
           return (
-            <YourCircle name={circle?.name} percentCompletion={circle?.completionPercentage} tasks={circle?.tasks} date={date} setReloadCounter={setReloadCounter}/>
+            <YourCircle name={circle?.name} percentCompletion={circle?.completionPercentage} tasks={circle?.tasks} date={date} setReloadCounter={setReloadCounter} time={circle?.time}/>
           )
         })
       }
