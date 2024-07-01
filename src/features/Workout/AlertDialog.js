8d48af89c0b9b4c36cb9a916e002fda56d079831
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-07-01 18:41:54 +0530

    Alert Dialog max-width set

--- src/features/Workout/AlertDialog.js
+++ src/features/Workout/AlertDialog.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const AlertDialog = ({ handleAlertDialog, message='Are you sure you want to submit?' }) => {
   return (
-    <div className="fixed inset-0 z-50  flex items-center justify-center bg-black bg-opacity-50 ">
-      <div className="rounded-lg border border-[#383838] bg-black  p-6 shadow-lg">
+    <div className="w-full mx-auto fixed inset-0 z-50  flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+      <div className=" max-w-[320px] rounded-lg border border-[#383838] bg-black  p-6 shadow-lg">
         <p className="mb-6 ">{message}</p>
         <div className="flex justify-end space-x-4">
           <button
