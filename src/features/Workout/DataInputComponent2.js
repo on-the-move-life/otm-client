3a9c2cf304d9f02f703775631f3cf9f36ab21515
Aviral Singh <aviralsingh360@gmail.com>
2024-07-12 00:42:32 +0530

    Max efforts Test section addition

--- src/features/Workout/DataInputComponent2.js
+++ src/features/Workout/DataInputComponent2.js
@@ -1,97 +1,108 @@
 import React, { useRef } from 'react';
 import useLocalStorage from '../../hooks/useLocalStorage';
 
-const DataInputComponent = ({
-    inputId,
-    inputType,
-    inputOptions,
-    placeholder,
-    label,
-    // value,
-    twClasses,
+const DataInputComponent2 = ({
+  inputId,
+  inputType,
+  inputOptions,
+  placeholder,
+  label,
+  index,
+  twClasses,
 }) => {
-    const selectInputRef = useRef('');
-    const textInputRef = useRef('');
-    const [storedValue, setValue, getItem] = useLocalStorage(inputId, '');
-    const [storedInputValues, setStoredInputValues, getStoredInputValues] = useLocalStorage('inputIds', []);
-    const handleInputChange = (event) => {
-        const newValue = event.target.value;
-        setValue(newValue);
+  const selectInputRef = useRef('');
+  const textInputRef = useRef('');
+  const [storedValue, setValue, getItem] = useLocalStorage(inputId, '');
+  const [storedInputValues, setStoredInputValues, getStoredInputValues] = useLocalStorage('inputIds', []);
 
-        // add the inputId to the storedInputValues array if it's not already there
-        const inputIds = getStoredInputValues();
-        if (inputIds !== null && !inputIds.includes(inputId)) {
-            setStoredInputValues([...inputIds, inputId]);
-        }
-        else if (inputIds === null) {
-            setStoredInputValues([inputId]);
-        }
-    };
+  const handleInputChange = (event) => {
+    const newValue = event.target.value;
+    setValue(newValue);
 
-    const inputDropdownStyle = {
-        backgroundColor: '#0F0F0F',
-        color: 'white',
-        width: '10px',
-    };
+    const inputIds = getStoredInputValues();
+    if (inputIds !== null && !inputIds.includes(inputId)) {
+      setStoredInputValues([...inputIds, inputId]);
+    } else if (inputIds === null) {
+      setStoredInputValues([inputId]);
+    }
+  };
 
-    twClasses = twClasses
-        ? twClasses
-        : 'w-full border-b border-gray-400 bg-transparent py-2 text-white outline-none focus:border-blue-500';
+  const inputDropdownStyle = {
+    backgroundColor: '#0F0F0F',
+    color: 'white',
+    width: '10px',
+  };
 
-    // Set default value to an empty string if not provided
-    const value = storedValue !== undefined ? storedValue : '';
+  twClasses = twClasses
+    ? twClasses
+    : 'w-full border-b border-gray-400 bg-transparent py-2 text-white outline-none focus:border-blue-500';
 
+  const value = storedValue !== undefined ? storedValue : '';
 
+  if (index === 0 || index === 1) {
     return (
-        <div className="py-4">
-            {inputType === 'select' ? (
-                <>
-                    <label
-                        className="text-xs tracking-widest text-lightGray"
-                        htmlFor={inputId}
-                    >
-                        {label}
-                    </label>
-
-                    <select
-                        className={twClasses}
-                        id={inputId}
-                        name={inputId}
-                        value={value}
-                        onChange={handleInputChange}
-                        label={label}
-                        ref={selectInputRef}
-                    >
-                        {inputOptions.map((option) => (
-                            <option key={option} value={option} style={inputDropdownStyle}>
-                                {option}
-                            </option>
-                        ))}
-                    </select>
-                </>
-            ) : inputType === 'textarea' ? (
-                <textarea
-                    className="textbox outline-none"
-                    id={inputId}
-                    name={inputId}
-                    value={value}
-                    onChange={handleInputChange}
-                    placeholder={placeholder}
-                    ref={textInputRef}
-                ></textarea>
-            ) : (
-                <input
-                    className="textbox outline-none"
-                    type={inputType}
-                    id={inputId}
-                    name={inputId}
-                    value={value}
-                    onChange={handleInputChange}
-                    placeholder={placeholder}
-                />
-            )}
-        </div>
+      <div className="py-4">
+        {inputType === 'select' ? (
+          <>
+            <label className="text-xs tracking-widest text-lightGray" htmlFor={inputId}>
+              {label}
+            </label>
+            <select
+              className={twClasses}
+              id={inputId}
+              name={inputId}
+              value={value}
+              onChange={handleInputChange}
+              label={label}
+              ref={selectInputRef}
+            >
+              {inputOptions.map((option) => (
+                <option key={option} value={option} style={inputDropdownStyle}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </>
+        ) : inputType === 'textarea' ? (
+          <textarea
+            className="textbox outline-none"
+            id={inputId}
+            name={inputId}
+            value={value}
+            onChange={handleInputChange}
+            placeholder={placeholder}
+            ref={textInputRef}
+          ></textarea>
+        ) : (
+          <input
+            className="textbox outline-none"
+            type={inputType}
+            id={inputId}
+            name={inputId}
+            value={value}
+            onChange={handleInputChange}
+            placeholder={placeholder}
+          />
+        )}
+      </div>
     );
+  } else if (index === 2) {
+    return (
+      <div className="mt-4">
+        <p className="mb-2 text-xs tracking-[3px] text-white">INSTRUCTIONS</p>
+        <ul className="list-disc pl-3">
+          <li className="my-2 text-xs font-light tracking-wider text-lightGray">
+            {placeholder}
+          </li>
+          <li className="my-2 text-xs font-light tracking-wider text-lightGray">
+            Do maximum 25 reps
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  return null;
 };
 
-export default DataInputComponent;
+export default DataInputComponent2;
