abff06166fe29bce5ac549a1f8c80abdfce2ed97
Aviral Singh <aviralsingh360@gmail.com>
2024-07-25 10:45:57 +0530

    Feedback changes

--- src/features/Workout/UpdateWorkout.js
+++ src/features/Workout/UpdateWorkout.js
@@ -54,56 +54,59 @@ const UpdateWorkout = ({ onClose }) => {
   };
 
   return (
-    <div className="overflow-x-auto">
-      <div className="h-screen w-screen flex-shrink-0 bg-[#141414] p-4 ">
-        <AnimatedComponent>
-          <div className="flex justify-end">
-            <span onClick={() => onClose(false)} className=' bg-[#202020] rounded-full p-1'>
-              <HiX size={15} />
-            </span>
-          </div>
-          <div className="flex flex-col justify-around">
-            <h3 className="relative top-5 workout-gradient-text text-center text-xl uppercase">
-              Customize your workout
-            </h3>
-            <p className='relative top-[60px] text-center sm:text-lg text-base'>Easily update your latest workout by selecting a theme, duration, and available equipment.</p>
-            <section className='relative top-20'>
-              <DataInputComponent
-                inputId="customTheme"
-                inputType="select"
-                inputOptions={WORKOUT_THEME_OPTIONS}
-                placeholder="select"
-                label="THEME"
-                twClasses="block w-full px-4 py-2 border border-[#2A2A2A] bg-transparent focus:outline-none rounded-lg border-[1px] border-[solid] border-[#2A2A2A] gap-[8px]"
-              />
-              <DataInputComponent
-                inputId="customEquipments"
-                inputType="select"
-                inputOptions={EQUIPMENT_OPTIONS}
-                placeholder="Choose equipment"
-                label="EQUIPMENT (OPTIONAL)"
-                twClasses="block w-full px-4 py-2 border border-[#2A2A2A] bg-transparent focus:outline-none rounded-lg border-[1px] border-[solid] border-[#2A2A2A] gap-[8px]"
-              />
-              <DataInputComponent
-                inputId="customDuration"
-                inputType="select"
-                inputOptions={WORKOUT_DURATION_OPTIONS}
-                placeholder="REGULAR"
-                label="WORKOUT DURATION"
-                twClasses="block w-full px-4 py-2 border border-[#2A2A2A] bg-transparent focus:outline-none rounded-lg border border-[#2A2A2A] gap-[8px]"
-              />
-            </section>
-
-            <button
-              className="relative top-60 workout-gradient-button h-11 w-full items-center  justify-center rounded-xl border border-[rgba(209,209,209,0.70)] text-xl font-bold text-black"
-              onClick={() => handleUpdateWorkout()}
-            >
-              Update
-            </button>
-          </div>
-        </AnimatedComponent>
-      </div>
+    <div className="relative h-screen w-screen overflow-y-auto bg-[#141414]">
+    <div className="p-4 pb-20">
+      <AnimatedComponent>
+        <div className="flex justify-end">
+          <span onClick={() => onClose(false)} className='bg-[#202020] rounded-full p-1'>
+            <HiX size={15} />
+          </span>
+        </div>
+        <div className="flex flex-col">
+          <h3 className="mt-5 workout-gradient-text text-center text-[24px] uppercase">
+            Customize your workout
+          </h3>
+          <p className='mt-4 text-center sm:text-lg text-[15px]'>
+            Easily update your latest workout by selecting a theme, duration, and available equipment.
+          </p>
+          <section className='mt-8'>
+            <DataInputComponent
+              inputId="customTheme"
+              inputType="select"
+              inputOptions={WORKOUT_THEME_OPTIONS}
+              placeholder="select"
+              label="THEME"
+              
+            />
+            <DataInputComponent
+              inputId="customEquipments"
+              inputType="select"
+              inputOptions={EQUIPMENT_OPTIONS}
+              placeholder="Choose equipment"
+              label="EQUIPMENT (OPTIONAL)"
+              
+            />
+            <DataInputComponent
+              inputId="customDuration"
+              inputType="select"
+              inputOptions={WORKOUT_DURATION_OPTIONS}
+              placeholder="REGULAR"
+              label="WORKOUT DURATION"
+              
+            />
+          </section>
+        </div>
+      </AnimatedComponent>
     </div>
+    <div className="fixed bottom-0 left-0 right-0 bg-[#141414] p-4">
+      <button
+        className="workout-gradient-button h-11 w-full items-center justify-center rounded-xl border border-[rgba(209,209,209,0.70)] text-xl font-bold text-black"
+        onClick={handleUpdateWorkout}
+      >
+        Update
+      </button>
+    </div>
+  </div>
   );
 };
 
