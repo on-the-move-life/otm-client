88badf3397518a3b4c071a22e5218accd1695705
Aviral Singh <aviralsingh360@gmail.com>
2024-08-28 15:46:12 +0530

    Updated service worker caching technique

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,27 +1,68 @@
-export function register() {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-        navigator.serviceWorker
-          .register(swUrl)
-          .then((registration) => {
-            console.log('Service Worker registered:', registration);
-          })
-          .catch((error) => {
-            console.error('Error registering Service Worker:', error);
-          });
-      });
-    }
-  }
+/* eslint-disable no-restricted-globals */
+const CACHE_NAME = `your-app-cache-${new Date().getTime()}`;
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/js/main.chunk.js",
+  "/static/js/0.chunk.js",
+  "/static/js/bundle.js",
+  "/static/css/main.chunk.css",
+  "/manifest.json"
+];
 
-  export function unregister() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.ready
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+export function register() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      navigator.serviceWorker
+        .register(swUrl)
         .then((registration) => {
-          registration.unregister();
+          console.log('Service Worker registered:', registration);
+          registration.update();
         })
         .catch((error) => {
-          console.error(error.message);
+          console.error('Error registering Service Worker:', error);
         });
-    }
+    });
   }
+}
+
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then((registration) => {
+        registration.unregister();
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
+}
