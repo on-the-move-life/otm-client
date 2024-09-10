c02ff3208746099771e61915b255700b1f75c6d3
Aviral Singh <aviralsingh360@gmail.com>
2024-09-10 16:58:54 +0530

    Feedback changes

--- src/components/InstallPWA.js
+++ src/components/InstallPWA.js
@@ -92,20 +92,20 @@ const InstallApp = () => {
           initial="hidden"
           animate="visible"
           variants={topPopupVariants}
-          className="fixed top-0 left-0 right-0 z-50 bg-black p-4 shadow-lg"
+          className="fixed top-0 left-0 right-0 z-50 bg-black-opacity-45 p-4 shadow-lg rounded-b-xl "
         >
           <div className="container mx-auto flex items-start justify-between">
             <div className="flex items-center space-x-4">
-              <img src="/assets/updated-logo.svg" alt="App Icon" className="w-12 h-12"/>
+              <img src="/assets/updated-logo.svg" alt="App Icon" className="w-10 h-10"/>
               <div>
-                <h2 className="text-xl font-[600] text-white">Install Our App</h2>
-                <p className="text-white sm:text-sm text-xs">Get the best experience on your device</p>
+                <h2 className="sm:text-lg font-[600] text-white text-[16px]">Install Our App</h2>
+                <p className="text-white sm:text-sm text-[12px]">Get the best experience on your device</p>
               </div>
             </div>
             <div className="flex items-center space-x-4">
               <button
                 onClick={handleInstallClick}
-                className="rounded-lg bg-blue px-4 py-2 text-black font-semibold"
+                className="rounded-lg bg-blue px-3 py-2 text-black font-semibold sm:text-sm text-[16px]"
               >
                 Install
               </button>
