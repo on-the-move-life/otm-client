79ae0d39c8d111fd70fdd2327587ffa41d7f8eef
Aakash Saxena <aakashspecs@gmail.com>
2025-01-24 10:48:33 +0530

    loader implemented

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,18 +3,15 @@ import React from 'react';
 const Loader = ({ className }) => {
   return (
     <div
-      className={`loading flex w-full flex-row items-center justify-center ${className !== undefined ? className : 'h-screen'}`}
+      className={` flex w-full flex-row items-center justify-center ${
+        className !== undefined ? className : 'h-screen'
+      }`}
     >
-      <svg width="64px" height="48px">
-        <polyline
-          points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"
-          id="back"
-        ></polyline>
-        <polyline
-          points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"
-          id="front"
-        ></polyline>
-      </svg>
+      <div class="loader">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </div>
     </div>
   );
 };
